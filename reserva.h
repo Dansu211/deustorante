3d773ac9e3671ada739518e54e3b782c8b44dc8c
EnekoBazan <eneko.bazan@opendeusto.es>
2025-03-27 11:10:50 +0100

    añadido reserva.h usuasrio.h

---
+++ reserva.h
@@ -0,0 +1,12 @@
+#ifndef reserva_h
+#define reserva_h
+
+typedef struct 
+{
+   int nPersonas;
+   char fecha[11];
+   char hora[6];
+
+}Reserva;
+
+#endif
---
+++ usuario.h
@@ -0,0 +1,11 @@
+#ifndef usuario_h
+#define usuario_h
+
+typedef struct
+{
+    char nombre;
+    char apellido;
+    int telf;
+}Usuario;
+
+#endif
